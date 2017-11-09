9af229187eeb670c35f0f6d27c03e47b6959676a
Aditya <adi@AdisMacAir.local>
2017-11-09 01:41:29 -0800

    adding some more functions

--- homework3.py
+++ homework3.py
@@ -1,25 +1,38 @@
-
 queries, sentences = [], []
 
-def getInputs():
-    with open('input.txt', 'r') as input_file:
+def getInputs(queries_list, sentences_list, filename):
+    with open(filename, 'r') as input_file:
         lines = input_file.readlines()
     no_of_queries = int(lines[0])
     no_of_sentences = int(lines[no_of_queries + 1])
     for query in range(1, no_of_queries + 1):
-        queries.append(lines[query].strip('\n'))
+        queries_list.append(lines[query].strip('\n'))
     for sentence in range(no_of_queries + 2, no_of_queries + 2 + no_of_sentences):
-        sentences.append(lines[sentence].strip('\n'))
+        sentences_list.append(lines[sentence].strip('\n'))
     pass
 
+def is_single_literal(sentence):
+    if len(sentence.split('|')) == 1:
+        return True
+    else:
+        return False
+
+def resolveIfLiteralPresent(query):
+    predicate = query.split('(')[0]
+    for sentence in sentences:
+        if is_single_literal(sentence) and predicate in sentence:
+            return True
+    return False
 
 def resolve(query):
     return resolveIfLiteralPresent(query) or resolveByImplication(query) or resolveByOrElimination(query)
 
-
-
 def findImplicationSentencesInWhichQueryExists(query):
-    pass
+    implicationSentences = []
+    for sentence in sentences:
+        if len(sentence.split('|')) == 2 and query in sentence.split('|')[1]:
+                implicationSentences.append(sentence)
+    return implicationSentences
 
 def negation(term):
     if '~' in term:
@@ -28,28 +41,38 @@ def negation(term):
         term = '~' + term
     return term
 
-def findMultipleDisjunctSentenceInWhichQueryExists(query):
+
+def getPredicate(query):
+    return query.split('(')[0]
+
+def findDNFInWhichQueryExists(query):
     for sentence in sentences:
-        if(getPredicate(query) in sentence):
+        if getPredicate(query) in sentence:
             disjunct_list = sentence.split('|')
-    neg_disjunct_list = map(negation, disjunct_list)
-    return disjunct_list
+    disjunct_list = list(map(str.strip, disjunct_list))[:-1]
+    neg_disjunct_list = list(map(negation, disjunct_list))
+    return neg_disjunct_list
 
 def resolveByOrElimination(query):
-    sentence = findMultipleDisjunctSentenceInWhichQueryExists(query) #TODO do for all such sentences
+    neg_disjuncts = findDNFInWhichQueryExists(query) #TODO do for all such sentences
+    for neg_disjunct in neg_disjuncts:
+        if not resolve(neg_disjunct):
+            return False
+    return True
+
+def get_premises(sentences):
+    premises = []
     for sentence in sentences:
-        terms = getNegDisjunctsFromDisjunction(sentence)
-        for term in terms:
-            if resolve(premise):
-                return True
-    return False
+        premises.append(negation(sentence.split('|')[0]).strip())
+    return premises
 
 def resolveByImplication(query):
-    premises = findImplicationSentencesInWhichQueryExists(query)
+    premises = get_premises(findImplicationSentencesInWhichQueryExists(query))
     for premise in premises:
         if resolve(premise):
             return True
     return False
 
 if __name__ == '__main__':
-    getInputs()
+    getInputs(queries, sentences, 'input.txt')
+    print(resolve('R(x)'))
--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import homework3
 
 class TestInference(TestCase):
     def test_getInputs(self):
-        homework3.getInputs()
+        homework3.getInputs(homework3.queries, homework3.sentences, 'input.txt')
         self.assertEqual(6, len(homework3.queries))
         self.assertEqual(14, len(homework3.sentences))
-        self.assertEqual('~W', homework3.negation('W'))
-        self.assertEqual('W', homework3.negation('~W'))
+        self.assertEqual(False, homework3.resolveByImplication('R(x)'))
+        self.assertEqual(False, homework3.resolveByOrElimination('R(x)'))
