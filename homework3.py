b6bd61baea5912d070d8242e2ab90c34ea35a317
Aditya <adi@AdisMacAir.local>
2017-11-09 21:47:36 -0800

    Added support for constants

--- homework3.py
+++ homework3.py
@@ -1,4 +1,5 @@
 queries, sentences = [], []
+KB = {}
 
 def getInputs(queries_list, sentences_list, filename):
     with open(filename, 'r') as input_file:
@@ -17,14 +18,30 @@ def is_single_literal(sentence):
     else:
         return False
 
+def isVariable(parameter):
+    list_of_parameters = parameter.split(',')
+    for parameter in list_of_parameters:
+        if parameter[0].isupper():
+            return False
+    return True
+
+
+def canUnify(parameter_in_sentence, parameter_in_query):
+    if len(parameter_in_query) != len(parameter_in_sentence):
+        return False
+
+
 def resolveIfLiteralPresent(query):
     predicate = query.split('(')[0]
+    parameter_in_query = query[query.find("(")+1:query.find(")")] # get parameter in ()
     for sentence in sentences:
-        if is_single_literal(sentence) and predicate in sentence:
-            return True
+        if is_single_literal(sentence) and predicate in sentence: # Get single literal sentences
+            parameter_in_sentence = sentence[sentence.find("(")+1:sentence.find(")")] #get parameter within ()
+            if isVariable(parameter_in_sentence) or parameter_in_query == parameter_in_sentence:
+                return True
     return False
 
-def resolve(query):
+def resolve(query): #TODO add DP
     return resolveIfLiteralPresent(query) or resolveByImplication(query) or resolveByOrElimination(query)
 
 def findImplicationSentencesInWhichQueryExists(query):
@@ -45,16 +62,26 @@ def negation(term):
 def getPredicate(query):
     return query.split('(')[0]
 
+
+def unificationNeeded(query, sentence):
+    pass #if query in sentence: #TODO is this needed?
+
+
 def findDNFInWhichQueryExists(query):
     for sentence in sentences:
-        if getPredicate(query) in sentence:
+        if getPredicate(query) in sentence and negation(getPredicate(query)) not in sentence:
+            if unificationNeeded(query, sentence):
+                pass#do something
             disjunct_list = sentence.split('|')
-    disjunct_list = list(map(str.strip, disjunct_list))[:-1]
-    neg_disjunct_list = list(map(negation, disjunct_list))
-    return neg_disjunct_list
+            disjunct_list = list(map(str.strip, disjunct_list))[:-1] # TODO need not be the last always
+            neg_disjunct_list = list(map(negation, disjunct_list))
+            return neg_disjunct_list
+    return []
 
 def resolveByOrElimination(query):
     neg_disjuncts = findDNFInWhichQueryExists(query) #TODO do for all such sentences
+    if not neg_disjuncts: # there is no sentence with multiple disjuncts
+        return False
     for neg_disjunct in neg_disjuncts:
         if not resolve(neg_disjunct):
             return False
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import homework3
 class TestInference(TestCase):
     def test_getInputs(self):
         homework3.getInputs(homework3.queries, homework3.sentences, 'input.txt')
-        self.assertEqual(6, len(homework3.queries))
-        self.assertEqual(14, len(homework3.sentences))
+        self.assertEqual(1, len(homework3.queries))
+        self.assertEqual(8, len(homework3.sentences))
         self.assertEqual(False, homework3.resolveByImplication('R(x)'))
         self.assertEqual(False, homework3.resolveByOrElimination('R(x)'))
