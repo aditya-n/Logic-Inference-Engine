9b71ad9f98a1c5a09c351ff88c3f802500cf3497
Aditya <adi@AdisMacAir.local>
2017-11-16 22:08:58 -0800

    unification changes

--- homework3.py
+++ homework3.py
@@ -1,4 +1,4 @@
-queries, sentences = [], []
+queries, KB_sentences_master, KB_sentences = [], [], []
 KB = {}
 
 def getInputs(queries_list, sentences_list, filename):
@@ -28,10 +28,16 @@ def isVariable(parameter):
 def getParameterFromTerm(term):
     return term[term.find("(") + 1:term.find(")")]
 
+
+def reverseMapping(map):
+    reverse_map = {v: k for k, v in map.items()}
+    return reverse_map
+
+
 def resolveIfLiteralPresent(query):
     predicate = query.split('(')[0]
     parameters_in_query = getParameterFromTerm(query) # get parameter in ()
-    for sentence in sentences:
+    for sentence in KB_sentences:
         if is_single_literal(sentence) and predicate in sentence: # Get single literal sentences
             parameters_in_sentence = getParameterFromTerm(sentence) #get parameter within ()
             if isVariable(parameters_in_sentence) or parameters_in_query == parameters_in_sentence:
@@ -41,10 +47,10 @@ def resolveIfLiteralPresent(query):
     return False
 
 def resolve(query): #TODO add DP
-    return resolveIfLiteralPresent(query) or resolveByImplication(query) or resolveByOrElimination(query)
+    return resolveIfLiteralPresent(query)  or resolveByOrEliminationForKB(query) #or resolveByImplication(query)
 
 def getVariableConstantPair(param1, param2):
-        return (param1, param2) if isVariable(param1) else (param2, param1)
+    return (param1, param2) if isVariable(param1) else (param2, param1)
 
 def getUnifierDict(query, term):
     unifiers = {}
@@ -56,39 +62,41 @@ def getUnifierDict(query, term):
         return {}
     no_of_params = len(query_params)
     for i in range(0, no_of_params):
-        if not (isVariable(term_params[i]) and isVariable(query_params[i])):
-            variable_constant_pair = getVariableConstantPair(term_params[i], query_params[i]) # to reorder as {variable : constant}
-            unifiers[variable_constant_pair[0]] = variable_constant_pair[1]
+        if not(term_params[i] == query_params[i]):
+            #variable_constant_pair = getVariableConstantPair(query_params[i], term_params[i]) # to reorder as {variable : constant} or {var1 : var2},
+            unifiers[query_params[i]] = term_params[i]                     # param order matters here for getVarConstPair()
     return unifiers
 
 def findImplicationSentencesInWhichQueryExists(query):
     implicationSentences, implication_sentences_with_unification = [], []
-    for sentence in sentences:
+    for sentence in KB_sentences:
         if len(sentence.split('|')) == 2: #Sentence capable of being in implication form
             conclusion = sentence.split('|')[1]
-            if getPredicate(query) in conclusion:
+            if getPredicate(query) in conclusion: # Sentence has query as conclusion
                 query_params = getParameterFromTerm(query)
                 conclusion_params = getParameterFromTerm(conclusion)
                 if query_params == conclusion_params or (isVariable(query_params) and isVariable(conclusion_params)):
                     implicationSentences.append(sentence) # When query matches conclusion in simple way
                 else:
-                    if not getUnifierDict(query, conclusion):
+                    if getUnifierDict(query, conclusion):
                         implication_sentences_with_unification.append(sentence) # When query matches conclusion after unification
 
     return implicationSentences, implication_sentences_with_unification
 
 def apply_unifiers(term, unifiers):
+    predicate = getPredicate(term)
+    params = getParameterFromTerm(term)
     for key in unifiers:
-        term = term.replace(key, unifiers[key])
-    return term
+        params = params.replace(key, unifiers[key])
+    return predicate + '(' + params + ')'
 
 def resolveByImplication(query):
     implication_sentences, implication_sentences_with_unification = findImplicationSentencesInWhichQueryExists(query)
     for implication_sentence in implication_sentences:
         premise, conclusion = get_premise_and_conclusion(implication_sentence)
-        result_resolve_premise = resolve(premise)
-        if result_resolve_premise:
-            return result_resolve_premise
+        result_of_resolve_premise_call = resolve(premise)
+        if result_of_resolve_premise_call:
+            return result_of_resolve_premise_call
 
     for implication_sentence in implication_sentences_with_unification:
         premise, conclusion = get_premise_and_conclusion(implication_sentence)
@@ -104,29 +112,71 @@ def negation(term): #TODO implement to solve neg(query)
 def getPredicate(query):
     return query.split('(')[0]
 
-def findDNFInWhichQueryExists(query):
-    for sentence in sentences: #TODO instead of looping all sentences, fetch from pre-indexed list where query is present
-        if getPredicate(query) in sentence and negation(getPredicate(query)) not in sentence:
-            #if unificationNeeded(query, sentence):
-                #pass#do something
-            disjunct_list = sentence.split('|')
-            disjunct_list = list(map(str.strip, disjunct_list))[:-1] # TODO need not be the last always
-            neg_disjunct_list = list(map(negation, disjunct_list))
-            return neg_disjunct_list
-    return []
 
-def resolveByOrElimination(query):
-    neg_disjuncts = findDNFInWhichQueryExists(query) #TODO do for all such sentences
+def getMatchingTerm(query, disjunct_list):
+    for disjunct in disjunct_list:
+        if getPredicate(query) == getPredicate(disjunct):
+            return disjunct
+    raise ValueError('Discrepancy in orElimination')
+
+
+def findDNFInWhichQueryExists(query, sentence):
+    if getPredicate(query) in sentence and negation(getPredicate(query)) not in sentence: #TODO Should solve even if just neg(query) present?
+        #if unificationNeeded(query, sentence):
+            #pass#do something
+        disjunct_list = sentence.split('|')
+
+        disjunct_list = list(map(str.strip, disjunct_list))
+        corresponding_disjunct = disjunct_list[-1]#getMatchingTerm(query, disjunct_list)
+        disjunct_list.remove(corresponding_disjunct)
+        neg_disjunct_list = list(map(negation, disjunct_list))
+        return neg_disjunct_list, corresponding_disjunct
+    return None, None
+
+
+def applyTransitiveOperation(pre_unifier_list, unifier_list):
+
+    for key in pre_unifier_list:
+        if pre_unifier_list[key] in unifier_list:
+            pre_unifier_list[key] = unifier_list[pre_unifier_list[key]]
+
+def removeVariableMappingsInUnifierList(pre_unifier_list):
+    return {key: value for key, value in pre_unifier_list.items() if not (isVariable(key) and isVariable(value))}
+
+def resolveByOrEliminationForKB(query):
+    for sentence in KB_sentences: #TODO instead of looping all sentences, fetch from pre-indexed list where query is present
+        resolve_result = resolveByOrElimination(query, sentence)
+        if resolve_result:
+            return resolve_result
+    return False
+
+
+def removeConstantAsKeyItems(unifier_list):
+    unifier_list_clone = unifier_list.copy() # cloning to prevent concurrent_modification_exception
+    for key in unifier_list:
+        if not isVariable(key):
+            del unifier_list_clone[key]
+    return unifier_list_clone
+
+def resolveByOrElimination(query, sentence):
+    #KB_sentences.remove(sentence)
+    neg_disjuncts, corresponding_disjunct = findDNFInWhichQueryExists(query, sentence)
     if not neg_disjuncts: # there is no sentence with multiple disjuncts
         return False
-    unifier_list = {}
+
+    pre_unifier_list = getUnifierDict(query, corresponding_disjunct)
+    unifier_list = reverseMapping(removeVariableMappingsInUnifierList(pre_unifier_list)) # filtering var-var unifications
+    unifier_list = removeConstantAsKeyItems(unifier_list)
     for neg_disjunct in neg_disjuncts:
+        neg_disjunct = apply_unifiers(neg_disjunct, unifier_list)
         result = resolve(neg_disjunct)
         if not result:
             return False
-        else:
+        elif not type(result) == type(True):
             unifier_list.update(result)
-    return unifier_list
+    applyTransitiveOperation(pre_unifier_list, unifier_list)
+    #pre_unifier_list = removeConstantAsKeyItems(pre_unifier_list)
+    return pre_unifier_list
 
 def get_premise_and_conclusion(implication_sentence):
     parts = implication_sentence.split('|')
@@ -134,5 +184,6 @@ def get_premise_and_conclusion(implication_sentence):
 
 
 if __name__ == '__main__':
-    getInputs(queries, sentences, 'input.txt')
-    print(resolve('R(x)'))
+    getInputs(queries, KB_sentences, 'input.txt')
+    for query in queries:
+        print(resolve(query))
--- test.py
+++ test.py
@@ -8,17 +8,22 @@ class TestInference(TestCase):
                                 '~H(x) | F(x)',
                                 'R(x, y)']
         self.assertEqual(False, homework3.resolveByImplication('R(x,y)'))
-        self.assertEqual(False, homework3.resolveByOrElimination('R(x,y)'))
+        self.assertEqual(False, homework3.resolveByOrEliminationForKB('R(x,y)'))
         self.assertEqual(True, homework3.resolveIfLiteralPresent('R(Yot,Joe)'))
         self.assertEqual({'x': 'John', 't': 'Missy'}, homework3.getUnifierDict('Rat(John,y,Missy)', 'Rat(x,z,t)'));
         self.assertEqual({'x': 'John', 't': 'Missy'}, homework3.getUnifierDict('Rat(x,z,t)', 'Rat(John,y,Missy)'));
         pass
 
     def test_unification(self):
-        homework3.queries, homework3.sentences = [], []
-        homework3.getInputs(homework3.queries, homework3.sentences, 'input.txt')
+        homework3.queries, homework3.KB_sentences = [], []
+        homework3.getInputs(homework3.queries, homework3.KB_sentences, 'input.txt')
         e = homework3.resolve('Missile(x)')
         d = homework3.resolveByImplication('Weapon(y)')
-        b = homework3.getUnifierDict('Criminal(x)', 'Criminal(x)')
-        a = homework3.resolve('Sells(West,M2,Nono)')
+        b = homework3.getUnifierDict('Sells(West,x,Nono)', 'Sells(West,M2,Nono)')
+        a1 = homework3.applyTransitiveOperation({'x':'y'}, {'y':'West'})
+        a11 = homework3.resolveByOrElimination('Sells(x,y,z)', '~Missile(x) | ~Owns(Nono,x) | Sells(West,x,Nono)');
+        a2 = homework3.resolveByOrEliminationForKB('Sells(x,y,z)')
+        a3 = homework3.resolve('Weapon(y)')
+        a34 = homework3.resolveByOrElimination('Sells(West,M1,z)', '~Missile(x) | ~Owns(Nono,x) | Sells(West,x,Nono)');
+        a4 = homework3.resolveByOrEliminationForKB('Criminal(West)')
         pass
