35d122993e09bc5608b258d131e7b6e2977e6b71
Aditya <adi@AdisMacAir.local>
2017-11-18 10:41:57 -0800

    Fixing tests

--- test.py
+++ test.py
@@ -20,6 +20,7 @@ class TestInference(TestCase):
         pass
 
     def test_2(self):
+        homework3.queries, homework3.KB_sentences = [], []
         homework3.getInputs(homework3.queries, homework3.KB_sentences, 'test2.txt')
         a = homework3.apply_unifiers('Ancestor(x,z)', {'x': 'Liz', 'z': 'Joe'})
 
@@ -27,15 +28,17 @@ class TestInference(TestCase):
         self.assertEqual(False, homework3.resolve('Ancestor(Liz,Joe)', set()));
 
     def test_1(self):
+        homework3.queries, homework3.KB_sentences = [], []
         homework3.getInputs(homework3.queries, homework3.KB_sentences, 'test1.txt')
 
         a = homework3.resolveIfLiteralPresent('B(John,y)');
         a = homework3.resolveIfLiteralPresent('D(Joe,y)');
-        self.assertEqual(False, homework3.resolve('F(Joe)', set()));
+
         a = homework3.resolve('B(John,x)', set());
         a = homework3.resolve('C(John,Joe)', set());
         a = homework3.resolve('A(John)', set());
 
+        self.assertEqual(False, homework3.resolve('F(Joe)', set()));
         self.assertNotEqual(False, homework3.resolve('H(John)', set()));
         self.assertNotEqual(False, homework3.resolve('~H(Alice)', set()));
         self.assertEqual(False, homework3.resolve('~H(John)', set()));
@@ -44,10 +47,12 @@ class TestInference(TestCase):
 
 
     def test_4(self):
+        homework3.queries, homework3.KB_sentences = [], []
         homework3.getInputs(homework3.queries, homework3.KB_sentences, 'test4.txt')
         self.assertEqual(False, homework3.resolve('Kills(Curiosity,Tuna)', set()));
 
     def test_5(self):
+        homework3.queries, homework3.KB_sentences = [], []
         homework3.getInputs(homework3.queries, homework3.KB_sentences, 'test5.txt')
         self.assertNotEqual(False, homework3.resolve('L(Tony,Snow)', set()));
 
